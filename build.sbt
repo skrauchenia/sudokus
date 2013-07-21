b2f8ea6643b312c40a39dd784bbd3d3e14e3bbba
Sergey Krauchenia <skrauchenia@exadel.com>
2013-07-22 00:35:43 +0300

    atom tests

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.9"
 scalaVersion := "2.10.2"
 
 libraryDependencies += "com.typesafe" % "config" % "1.0.2"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
---
+++ src/test/scala/com/kupal/sudokus/process/model/AtomTest.scala
@@ -0,0 +1,35 @@
+package com.kupal.sudokus.process.model
+
+import org.scalatest.{FunSpec, GivenWhenThen, FeatureSpec}
+
+/**
+ * @author skrauchenia
+ */
+class AtomTest extends FunSpec {
+
+  describe("An Empty atom") {
+
+    it("should return zero as value") {
+      val empty = Atom.create(4, 0)
+      assert(empty.value == 0)
+    }
+
+    it("should be indicated as unresolved") {
+      val empty = Atom.createEmpty(4)
+      assert(!empty.isResolved())
+    }
+  }
+
+  describe("Resolved atom") {
+
+    it("should return provided value") {
+      val atom = Atom.create(5, 10)
+      assert(atom.value == 10)
+    }
+
+    it("should be indicated as resolved") {
+      val atom = Atom.create(5, 10)
+      assert(atom.isResolved())
+    }
+  }
+}
